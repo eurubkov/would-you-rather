fd7e1e95d68d2c3cbccac18d11b4a9184b9da738
= <10166308+eurubkov@users.noreply.github.com>
2021-06-29 17:48:28 -0700

    feat: Add UnansweredQuestion component, actions and reducers for adding answers

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,11 +1,25 @@
-import { _saveQuestion } from "../utils/_DATA";
+import { _saveQuestion, _saveQuestionAnswer } from "../utils/_DATA";
 
 export const ADD_QUESTION = "ADD_QUESTION";
+export const ADD_ANSWER = "ADD_ANSWER";
+export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
 
 const addQuestion = (question) => ({
   type: ADD_QUESTION,
   question,
 });
+
+const addAnswer = (answer) => ({
+  type: ADD_ANSWER,
+  answer,
+});
+
+export const receiveQuestions = (questions) => {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions,
+  };
+};
 
 export const handleAddQuestion =
   (option1, option2, author) => async (dispatch, getState) => {
@@ -15,4 +29,10 @@ export const handleAddQuestion =
       author,
     });
     return dispatch(addQuestion(question));
+  };
+
+export const handleAddAnswer =
+  (authedUser, qid, answer) => async (dispatch, getState) => {
+    await _saveQuestionAnswer({ authedUser, qid, answer });
+    return dispatch(addAnswer({ authedUser, qid, answer }));
   };
--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,12 @@
-import { _getUsers } from "../utils/_DATA";
+import { _getUsers, _getQuestions } from "../utils/_DATA";
 import { receiveUsers } from "../actions/users";
+import { receiveQuestions } from "../actions/questions";
 
 export const handleInitialData = () => {
   return async (dispatch) => {
     const users = await _getUsers();
+    const questions = await _getQuestions();
     dispatch(receiveUsers(users));
+    dispatch(receiveQuestions(questions));
   };
 };
