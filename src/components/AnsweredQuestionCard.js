7ad3975dce3715e0b4c066f5de471874717aea5a
= <10166308+eurubkov@users.noreply.github.com>
2021-07-05 17:23:31 -0700

    refactor: Make QuestionCard a base component that can render children

--- src/components/AnsweredQuestionCard.js
+++ src/components/AnsweredQuestionCard.js
@@ -1,31 +1,18 @@
 import { React } from "react";
-import { Card, CardContent, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: "250px",
-    minHeight: "200px",
-  },
-});
+import { Typography } from "@material-ui/core";
+import QuestionCard from "./QuestionCard";
 
 const AnsweredQuestionCard = ({
   questionText,
   votesCount,
   votesPercentage,
 }) => {
-  const classes = useStyles();
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {questionText}
-        </Typography>
-        <Typography color="textSecondary">
+    <QuestionCard questionText={questionText}>
+      <Typography color="textSecondary">
           {votesCount} ({votesPercentage}%) people voted for this option
         </Typography>
-      </CardContent>
-    </Card>
+    </QuestionCard>
   );
 };
 
