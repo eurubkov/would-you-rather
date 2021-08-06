b28ba8ffee216574c3bd5c0fddc3dab2a6733c43
= <10166308+eurubkov@users.noreply.github.com>
2021-08-05 21:08:59 -0700

    refactor: - Change Leaderboard page to meet the requirements

--- src/components/LeaderboardUser.js
+++ src/components/LeaderboardUser.js
@@ -1,12 +1,21 @@
 import { React } from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import Avatar from "@material-ui/core/Avatar";
 
 const LeaderboardUser = ({ user }) => {
+  const asked = user.questions.length;
+  const answered = Object.keys(user["answers"]).length;
   return (
-    <div>
-      <h5>{user["name"]}</h5>
-      <h5>Questions Asked: {user["questions"].length}</h5>
-      <h5>Questions Answered: {Object.keys(user["answers"]).length}</h5>
-    </div>
+    <TableRow key={user.name}>
+      <TableCell component="th" scope="row">
+        <Avatar src={user.avatarURL} alt="user avatar" />
+      </TableCell>
+      <TableCell>{user.name}</TableCell>
+      <TableCell>{asked}</TableCell>
+      <TableCell>{answered}</TableCell>
+      <TableCell>{asked + answered}</TableCell>
+    </TableRow>
   );
 };
 
