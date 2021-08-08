a7e2ef507aa67a31bce01a2fedf4b226a50b0060
= <10166308+eurubkov@users.noreply.github.com>
2021-08-08 12:44:46 -0700

    fix: - Add Would You Rather text - Add user avatar to unanswered questions

--- src/components/UnansweredQuestion.js
+++ src/components/UnansweredQuestion.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import QuestionCard from "./QuestionCard";
 import Button from "@material-ui/core/Button";
 import { withRouter, Link } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles({
   root: {
@@ -17,16 +18,18 @@ const useStyles = makeStyles({
   },
 });
 
-const UnansweredQuestion = ({ dispatch, authedUser, question }) => {
+const UnansweredQuestion = ({ dispatch, authedUser, question, users }) => {
   const classes = useStyles();
+  console.log(authedUser);
   if (question === null) {
     return <p>This question doesn't exist.</p>;
   }
   const { optionOne, optionTwo, author, id } = question;
+  const avatarURL = users[author].avatarURL;
 
   return (
     <>
-      <h6 style={{ textAlign: "center" }}>Question provided by {author}</h6>
+      <h1 style={{ textAlign: "center" }}>Would You Rather</h1>
       <div className={classes.container}>
         <QuestionCard questionText={optionOne.text} />
         <h1 style={{ marginLeft: "10px", marginRight: "10px" }}>OR</h1>
@@ -41,13 +44,27 @@ const UnansweredQuestion = ({ dispatch, authedUser, question }) => {
           Answer
         </Button>
       </Link>
+      <div
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        <h6 style={{ textAlign: "center" }}>Question provided by {author}</h6>
+        <Avatar src={avatarURL} alt="user avatar" />
+      </div>
     </>
   );
 };
 
-const mapStateToProps = ({ authedUser, questions }, { id }) => {
+const mapStateToProps = ({ authedUser, questions, users }, { id }) => {
   const question = questions[id];
-  return { authedUser, question: question ? question : null };
+  return {
+    authedUser,
+    question: question ? question : null,
+    users,
+  };
 };
 
 export default withRouter(connect(mapStateToProps)(UnansweredQuestion));
